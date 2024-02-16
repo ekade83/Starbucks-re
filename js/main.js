f999401221d1b22f47a0b7077fdbd397d7e08539
ekade83 <ekade@naver.com>
2024-02-16 14:09:02 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 "use strict";
 
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 /**
  * 페이지 스크롤에 따른 요소 제어
  */
@@ -181,9 +164,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
-
-/**
- * 올해가 몇 년도인지 계산
- */
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
